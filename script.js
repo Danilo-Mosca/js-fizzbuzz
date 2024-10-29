d01071ca1a93f08830aa036796bfa37b8655514d
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-10-29 14:36:42 +0100

    create index.html, script.js and variable declaration

---
+++ script.js
@@ -0,0 +1,12 @@
+// Utlizzo la direttiva "use strict" per far eseguire il codice in maniera rigorosa
+"use strict";
+// Ripulisco la console
+console.clear();
+
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+//creo e dichiaro le variabili
+numeri = 100;
