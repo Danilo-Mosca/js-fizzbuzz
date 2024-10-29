35dddae62ba7db84f92492b79344850ffe66aa17
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-10-29 16:12:41 +0100

    add switch in the cicle

--- script.js
+++ script.js
@@ -36,3 +36,23 @@ for (let i = 1; i <= numeri; i++) {
     }
 
 }
+
+// Soluzione alternativa usando switch
+console.log("Soluzione alternativa con lo 'switch':");
+
+for (let i = 1; i <= numeri; i++) {
+
+    switch (true) {
+        case ((i % 3 === 0) && (i % 5 === 0)):
+            console.log("FizzBuzz");
+            break;
+        case (i % 3 === 0):
+            console.log("Fizz");
+            break;
+        case (i % 5 === 0):
+            console.log("Buzz");
+            break;
+        default:
+            console.log(i);
+    }
+}
